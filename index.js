40cdee46cb89e25016be91a3256a488174cfdd5a
ezraschwepker@gmail.com <ezraschwepker@gmail.com>
2018-09-10 04:47:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,5 +4,18 @@ let recipes {
 };
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  let newObj = Object.assign({}, object)
+  let newObj = Object.assign({}, object);
+  newObj[key] = value;
+  return newObj;
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
+}
+
+function deleteFromObjectByKey(object, key) {
+  let newObj = Object.assign({}, object);
+  delete newObj[key];
+  return newObj;
+} 
